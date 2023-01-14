342830fb4fb1f2717b0a940f8e7b09b9c6b2fd63
John Turco <101477820+JohnVTurco@users.noreply.github.com>
2023-01-14 16:57:50 -0500

    Add files via upload

---
+++ war/war/warApp.swift
@@ -0,0 +1,17 @@
+//
+//  warApp.swift
+//  war
+//
+//  Created by John Turco on 2022-12-28.
+//
+
+import SwiftUI
+
+@main
+struct warApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
